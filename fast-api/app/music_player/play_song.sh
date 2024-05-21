57ececa5458db87ba83caabf73964acef39aeeac
Sandro Räss <s.raess@me.com>
2024-05-21 14:20:29 +0200

    Add nohup to play_song.sh.

--- fast-api/app/music_player/play_song.sh
+++ fast-api/app/music_player/play_song.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 ./clean_up.sh
-mpg123 music_library/$1 &
+nohup mpg123 music_library/$1 &
 #cvlc music_library/$1 --play-and-exit --novideo &
 echo $! >current_pid
